7d95f09664777f6358c3d9c8ca6e802983d01dfc
same7 <same7@Sameh-Dell-L502X>
2018-04-21 11:57:05 +0200

    add base starting code

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=db2task
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/db2task
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=db2task.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/db2task.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=db2task
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/db2task
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=db2task.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/db2task.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ source/main.cpp
@@ -0,0 +1,109 @@
+#include <string>
+#include <core/global_definitions.hpp>
+#include <core/base_column.hpp>
+#include <core/column_base_typed.hpp>
+#include <core/column.hpp>
+#include <core/compressed_column.hpp>
+
+/*this is the include for the example compressed column with empty implementation*/
+#include <compression/dictionary_compressed_column.hpp>
+
+#include  "unittest.hpp"
+
+using namespace CoGaDB;
+
+int main() {
+    /*Adapt the Column to your implemented method*/
+    std::cout << "Dic: " << std::endl;
+    if (!unittest<Column, int>()) {
+        std::cout << "At least one Unittest Failed!" << std::endl;
+        return -1;
+    }
+    std::cout << "Unitests Passed!" << std::endl;
+
+    if (!unittest<Column, float>()) {
+        std::cout << "At least one Unittest Failed!" << std::endl;
+        return -1;
+    }
+    std::cout << "Unitests Passed!" << std::endl;
+
+    if (!unittest<Column, std::string>()) {
+        std::cout << "At least one Unittest Failed!" << std::endl;
+        return -1;
+    }
+    std::cout << "Unitests Passed!" << std::endl;
+
+    //	/****** BULK UPDATE TEST ******/
+    //	{
+    //		std::cout << "BULK UPDATE TEST..."; // << std::endl;
+    //		boost::shared_ptr<Column<int> > uncompressed_col (new Column<int>("int column",INT));
+    //		boost::shared_ptr<Column<int> > compressed_col (new Column<int>("int column",INT));
+    //		//boost::shared_ptr<DictionaryCompressedColumn<int> > compressed_col (new DictionaryCompressedColumn<int>("compressed int column",INT));
+
+
+    //		uncompressed_col->insert(reference_data.begin(),reference_data.end()); 
+    //		compressed_col->insert(reference_data.begin(),reference_data.end()); 
+
+    //		bool result = *(boost::static_pointer_cast<ColumnBaseTyped<int> >(uncompressed_col))==*(boost::static_pointer_cast<ColumnBaseTyped<int> >(compressed_col));
+    //		if(!result){ 
+    //			std::cerr << std::endl << "operator== TEST FAILED!" << std::endl;	
+    //			return false;
+    //		}
+    //		PositionListPtr tids (new PositionList());
+    //		int new_value=rand()%100;
+    //	   for(unsigned int i=0;i<10;i++){
+    //	 		tids->push_back(rand()%uncompressed_col->size());
+    //	   }
+    //		
+    //		uncompressed_col->update(tids,new_value); 
+    //		compressed_col->update(tids,new_value); 
+
+    //		result = *(boost::static_pointer_cast<ColumnBaseTyped<int> >(uncompressed_col))==*(boost::static_pointer_cast<ColumnBaseTyped<int> >(compressed_col));
+    //		if(!result){
+    //			 std::cerr << std::endl << "BULK UPDATE TEST FAILED!" << std::endl;	
+    //			 return false;	
+    //		}
+    //		std::cout << "SUCCESS"<< std::endl;	
+
+    //	}
+
+    //	/****** BULK DELETE TEST ******/
+    //	{
+    //		std::cout << "BULK DELETE TEST..."; // << std::endl;
+    //		boost::shared_ptr<Column<int> > uncompressed_col (new Column<int>("int column",INT));
+    //		boost::shared_ptr<Column<int> > compressed_col (new Column<int>("int column",INT));
+
+    //		//boost::shared_ptr<DictionaryCompressedColumn<int> > compressed_col (new DictionaryCompressedColumn<int>("compressed int column",INT));
+
+    //		uncompressed_col->insert(reference_data.begin(),reference_data.end()); 
+    //		compressed_col->insert(reference_data.begin(),reference_data.end()); 
+
+    //		bool result = *(boost::static_pointer_cast<ColumnBaseTyped<int> >(uncompressed_col))==*(boost::static_pointer_cast<ColumnBaseTyped<int> >(compressed_col));
+    //		if(!result){ 
+    //			std::cerr << std::endl << "operator== TEST FAILED!" << std::endl;	
+    //			return false;
+    //		}
+
+    //		PositionListPtr tids (new PositionList());
+
+    //	   for(unsigned int i=0;i<10;i++){
+    //	 		tids->push_back(rand()%uncompressed_col->size());
+    //	   }
+    //		
+    //		uncompressed_col->remove(tids); 
+    //		compressed_col->remove(tids); 
+
+    //		result = *(boost::static_pointer_cast<ColumnBaseTyped<int> >(uncompressed_col))==*(boost::static_pointer_cast<ColumnBaseTyped<int> >(compressed_col));
+    //		if(!result){
+    //			 std::cerr << "BULK DELETE TEST FAILED!" << std::endl;	
+    //			 return false;	
+    //		}
+    //		std::cout << "SUCCESS"<< std::endl;	
+
+    //	}
+
+
+    return 0;
+}
+
+
